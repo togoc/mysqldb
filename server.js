11a893243133c37211cbf3bf6e8ab0711e3eba30
togoc <49116563+togoc@users.noreply.github.com>
2019-10-17 23:11:01 +0800

    update

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const fs = require("fs")
 const app = express()
-//两种连接方式
+const bodyparser = require('body-parser');
+// 在Express 中 没有内置获取表单POST请求体的API，这里我们需要使用一个第三方的包 ：body-parser
+//req.body 识别
+app.use(bodyparser.urlencoded({ extende: true }));
+app.use(bodyparser.json())
+    //两种连接方式
 const pool = require("./mysqldb/pool")
 const terminate = require("./mysqldb/terminate")
 
@@ -22,6 +27,8 @@ app.get("/table", (req, res) => {
         pool(sql).then((re) => {
             res.send({ code: 1, data: re })
         })
+    } else if (req.query.code == "insert") {
+        console.log(req.query)
     } else {
         pool(sql).then((re) => {
             pool("select * from students").then((re) => {
@@ -33,14 +40,16 @@ app.get("/table", (req, res) => {
 
 })
 
-
-
-
-app.listen(8080, function () {
-    console.log("port 8080 !")
+app.post("/insert", (req, res) => {
+    pool(req.query.sentence, req.body).then((re) => {
+        pool("select * from students").then((re) => {
+            res.send({ code: 1, data: re })
+        })
+    })
 })
 
 
 
-
-
+app.listen(8080, function() {
+    console.log("port 8080 !")
+})
--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,15 @@
+let arr = []
 let dic = []
 
 
-// let login_tip_button = document.querySelector('.login_tip_button input')
-// login_tip_button.addEventListener('click',function(){
-//     console.log('login_tip_button')
-// })
-
-
-
 // 滚动进化
-window.addEventListener("mousewheel", function (event) {
+window.addEventListener("mousewheel", function(event) {
     /**
      * 0
      * 1002
      */
     let clientHeight = document.body.clientHeight
     setTimeout(() => {
-        // console.log(scrollY)
-        console.log(clientHeight)
         if (event.wheelDeltaY < 0 && scrollY > 0 && scrollY < clientHeight / 4)
             scrollTo(0, clientHeight / 2)
         if (event.wheelDeltaY > 0 && scrollY > clientHeight / 4)
@@ -38,34 +30,29 @@ function toggleClassPlu(elem, name) {
 }
 let selectItems = document.querySelectorAll('.select a')
 for (let i = 0; i < selectItems.length; i++) {
-    selectItems[i].addEventListener("click", function () {
+    selectItems[i].addEventListener("click", function() {
         toggleClassPlu(selectItems, "select_active")
         this.classList.add("select_active")
-        console.log(this.id)
         switch (this.id) {
             case "students":
-                // let t = `  
-                // <col width="10%" height="40px">
-                // <col width="10%" height="40px">
-                // <col width="10%" height="40px">
-                // <col width="10%" height="40px">
-                // <col width="20%" height="40px">
-                // <col width="20%" height="40px">
-                // <col width="20%" height="40px">`
-                // $('table').append(t)
-                // console.log($('col').remove())//删除所有
-                getTable("table?code=select&sentence=select * from students")
+                getTable("students");
                 break;
             case "teacher":
-                getTable("table?code=select&sentence=select * from teacher")
+                getTable("teacher")
                 break;
             case "course":
-                getTable("table?code=select&sentence=select * from course")
+                getTable("course")
                 break;
-            default: alert("错误");
+            default:
+                alert("错误");
         }
     })
 }
+
+
+
+
+
 
 //查询
 
@@ -80,10 +67,13 @@ function ajax(url, type = "get", data = {}, processData = true, contentType = tr
     let method = { url, type, data, processData, contentType }
     if (method.contentType)
         delete method.contentType
+    if (method.processData) {
+        delete method.processData
+    }
     $(".background").show()
     return new Promise((resolve, reject) => {
         $.ajax(method).done(
-            function (res) {
+            function(res) {
                 console.log(res.data)
                 dic = res.data
                 resolve(res)
@@ -93,179 +83,324 @@ function ajax(url, type = "get", data = {}, processData = true, contentType = tr
     })
 }
 
-function getTable(url) {
+
+function check(str) {
+    if (str.indexOf("students") != -1) {
+        location.hash = "/p=students";
+        $('.select').children().find("a")[0].classList.add("select_active")
+    }
+    if (str.indexOf("teacher") != -1) {
+        location.hash = "/p=teacher";
+        $('.select').children().find("a")[1].classList.add("select_active")
+    }
+    if (str.indexOf("course") != -1) {
+        location.hash = "/p=course";
+        $('.select').children().find("a")[2].classList.add("select_active")
+    }
+}
+
+
+function getTable(str) {
+    //重载
+    let url = ""
+    if (str == "") {
+        if (!localStorage.getItem("table")) {
+            url = "table?code=select&sentence=select * from students"
+        } else {
+            url = "table?code=select&sentence=select * from " + localStorage.getItem("table")
+        }
+    } else {
+        url = "table?code=select&sentence=select * from " + str
+        localStorage.setItem("table", str)
+    }
     ajax(url).then((res) => {
-        let t = ""
-        let arr = []
+        // 修改hash
+        check(url)
+        let tr = ""
+        let td = ""
+        let titleArr = []
+        arr = []
         for (const i in dic[0]) {
             arr.push(i)
         }
-        console.log(arr)
-        for (let i = 0; i < dic.length; i++) {
-            t +=
-                `
-                <tr align="left">
-                                <td class="${arr[0]}">
-                                    <div class="td_container">
-                                        <span>${dic[i][arr[0]]}</span>
-                                    </div>
-                                </td>
-                                <td class="${arr[1]}">
-                                    <div class="td_container">
-                                        <span>${dic[i][arr[1]]}</span>
-                                    </div>
-                                </td>
-                                <td class="${arr[2]}">
-                                    <div class="td_container">
-                                        <span>${dic[i][arr[2]]}</span>
-                                    </div>
-                                </td>
-                                <td class="${arr[3]}">
-                                    <div class="td_container">
-                                        <span>${dic[i][arr[3]]}</span>
-                                    </div>
-                                </td>
-                                <td class="${arr[4]}">
-                                    <div class="td_container">
-                                        <span>${dic[i][arr[4]]}</span>
-                                        <i class="edit"></i>
-                                        </div>
-                                </td>
-                                <td class="${arr[5]}">
-                                    <div class="td_container">
-                                        <span>${dic[i][arr[5]]}</span>
-                                        <i class="edit"></i>
-                                        </div>
-                                </td>
-                                <td class="${arr[6]}">
-                                    <div class="td_container">
-                                        <span>${dic[i][arr[6]]}</span>
-                                        <i class="edit"></i>
-                                        </div>
-                                </td>
-                            </tr>
-                `
-        }
-        $('.table').html(t)
-        let allTr = $("tr")
         arr.map((val, index) => {
-            if (val == "name")
-                val = "姓名"
-            if (val.indexOf("id") != -1)
-                (val == "sid") ? (val = "学号") : ((val == "tid") ? (val = "工号") : (val = "课号"));
-            if (val == 'age')
-                val = "年龄"
-            if (val == 'gender')
-                val = "性别"
-            if (val == 'phone_number')
-                val = "电话"
-            if (val == 'address')
-                val = "住址"
-            if (val == 'remark')
-                val = "备注"
-            if (val == 'teacher')
-                val = "任课教师"
-            arr[index] = val
-        })
-
-        for (let i = 0; i < allTr.length; i++) {
-            for (let x = 0; x < arr.length; x++) {
-                $(allTr[i].children[x]).show()
-                $($("tr")[0]).children()[x].innerHTML = arr[x]
+                if (val.indexOf("name") != -1)
+                    (val == "name") ? (val = "姓名") : (val = "课程名称")
+                if (val.indexOf("id") != -1)
+                    (val == "sid") ? (val = "学号") : ((val == "tid") ? (val = "工号") : (val = "课号"));
+                if (val == 'age')
+                    val = "年龄"
+                if (val == 'gender')
+                    val = "性别"
+                if (val == 'phone_number')
+                    val = "电话"
+                if (val == 'address')
+                    val = "住址"
+                if (val == 'remark')
+                    val = "备注"
+                if (val == 'teacher')
+                    val = "任课教师"
+                titleArr[index] = val
+            })
+            //清空表头再插入
+        $(".thead").html("")
+        for (let i = 0; i < titleArr.length; i++) {
+            tr = `<th class="${arr[i]}">${titleArr[i]}</th>`
+            $(".thead").append(tr)
+        }
+        //清空table再插入
+        $(".table").html(" ")
+        for (let i = 0; i < dic.length; i++) {
+            if (i < dic.length)
+                tr = `<tr align="left"></tr>`
+            $(".table").append(tr)
+            for (let j = 0; j < arr.length; j++) {
+                td = `
+                <td class="${arr[j]}">
+                    <div class="td_container">
+                        <span>${dic[i][arr[j]]}</span>
+                        <i class="edit"></i>
+                    </div>
+                </td>
+                `
+                $($(".table").children()[i]).append(td)
             }
         }
-        for (let i = 0; i < allTr.length; i++) {
-            for (let j = 6; j >= arr.length; j--) {
-                $(allTr[i].children[j]).hide()
+
+
+        //添加insert按钮并添加行
+
+        tr = `
+            <tr align="center">
+                <td class="insert" colspan="${arr.length}">添加 + </td>
+            </tr>
+        `
+        $(".table").append(tr)
+        insert()
+
+    })
+}
+
+function insert() {
+    let obj = Object.create(dic[0])
+    $(".insert").click((event) => {
+        if (event.target.innerHTML === "添加 + " && event.target.className === "insert") {
+            if (document.querySelector(".change_input")) {
+                alert("请输入")
+                return
             }
+            tr = `<tr align="left" class="add_row"></tr>`
+            $(event.target.closest('tr')).before(tr)
+            for (let i = 0; i < arr.length; i++) {
+                td = `
+                <td class="${arr[i]}">
+                    <div class="change_bar">
+                        <input class="change_input" type="text" placeholder="广西">
+                    </div>
+                </td>
+                 `
+                $(".add_row").append(td)
+            }
+            $(".insert").html("确认")
+
+
+            for (const key in obj) {
+                obj[key] = ""
+            }
+            document.querySelector('.table').addEventListener('change', function(ev) {
+                obj[ev.target.closest("td").className] = ev.target.value
+            })
+
+        } else if (event.target.innerHTML === "确认" && event.target.className === "insert") {
+            for (const key in obj) {
+                obj[key] = obj[key].replace(/(^\s*)|(\s*$)/g, "").replace(/<|>|%|\\|"|=|'|{|}|!|\?|\(|\)|\*/, "-")
+                if (key.indexOf("id") != -1) {
+                    if (obj[key] == "") {
+                        alert("id不能空")
+                        return
+                    }
+                    if (Boolean(Number(obj[key]))) {
+                        for (let i = 0; i < dic.length; i++) {
+                            if (dic[i][key] == Number(obj[key])) {
+                                alert("id重复请重新输入")
+                                return
+                            }
+                            obj[key] = Number(obj[key])
+                        }
+                    } else {
+                        alert("id必须为数字int")
+                        return
+                    }
+                }
+                if (obj[key] != "") {
+                    console.log(key)
+                    if (key.indexOf("age") != -1 || key.indexOf("phone_number") != -1) {
+
+                        if (!Boolean(Number(obj[key]))) {
+                            alert("年龄/电话号码必须为数字")
+                            return
+                        }
+                    }
+                }
+                if (obj[key] == "") {
+                    delete obj[key]
+                    delete obj.__proto__[key]
+                }
+            }
+            let table = $(".select_active").attr('id')
+            let url = `/insert?code=insert&sentence=insert into ${table} set ?`
+            document.querySelector('.table').removeEventListener('change', function(ev) {
+                obj[ev.target.closest("td").className] = ev.target.value
+            })
+            console.log(obj)
+            ajax(url, type = "post", obj, true, true).then((res) => {
+                console.log(obj)
+                if (res.code === 1)
+                    alert("添加成功")
+                location.reload()
+            })
         }
     })
 }
 
 
+// ajax(url, type = "post", {name}, true, true).then((res) => {
+//     console.log(obj)
+//     if (res.code === 1)
+//         alert("添加成功")
+//         // location.reload()
+// })
+let obj = { name: 11 }
+obj.__proto__.name = 22
+delete obj.__proto__.name
+delete obj.name
+console.log(obj.name)
+
+// 检查输入格式
+function format(obj) {
+    for (const key in obj) {
+        obj[key] = obj[key].replace(/(^\s*)|(\s*$)/g, "").replace(/<|>|%|\\|"|=|'|{|}|!|\?|\(|\)|\*/, "-")
+        if (key.indexOf("id") != -1) {
+            if (obj[key] == "") {
+                alert("id不能空")
+                return
+            }
+
+            if (Boolean(Number(obj[key]))) {
+                for (let i = 0; i < dic.length; i++) {
+                    if (dic[i][key] == Number(obj[key])) {
+                        alert("id重复请重新输入")
+                        return
+                    }
+                    obj[key] = Number(obj[key])
+                }
+            } else {
+                alert("id必须为数字int")
+                return
+            }
+        }
 
 
-// function connectTable(sql, options = {}) {
-//     let options = {}
-//     let sql = ""
-//     let url = "table?code=select&sentence=select * from students"
+        // if (obj.hasOwnProperty(key)) {
+        //     const element = obj[key];
 
-// }
+        // }
+    }
 
+}
+let str = "w   ( "
+str = str.replace(/\s/g, "")
+console.log(str)
 
-
-
-
-
-
-
-
-
-
-
-
-
-$(".table").click(function (event) {
+$(".table").click(function(event) {
     if (event.target.nodeName == "I") {
         let id = $(event.target.closest("tr").children[0]).find('span').html()
         let rootid = $(event.target.closest("tr").children[0]).attr('class')
         let item = event.target.closest("td").className
         let tips = $(event.target).prev().html()
         let par = $(event.target.closest("td"))
-        par.children().remove()
-        let inpu = `
-                    <div class="change_bar">
-                        <input class="change_input" type="text"  placeholder="${tips}">
-                        <input class="change_btn" type="button" value="确认">
-                    </div>
-                   `
-        par.html(inpu)
+            // if (document.querySelector('.add_row')) {
+            //     alert("请输入")
+            //     return
+            // }
+        if (document.querySelector('.change_input')) {
+            alert("请修改")
+            return
+        } else {
+            par.children().remove()
+            let inpu = `
+            <div class="change_bar">
+                <input class="change_input" type="text"  placeholder="${tips}">
+                <input class="change_btn" type="button" value="确认">
+            </div>
+           `
+            par.html(inpu)
+        }
+
         let change_input = document.querySelector('.change_input')
         let change_btn = document.querySelector('.change_btn')
-        if (change_input) {
-            change_input.oninput = function () {
-                change_btn.title = change_input.value
-            }
-            change_btn.addEventListener('click', function () {
-                let text = change_input.value.replace(/(^\s*)|(\s*$)/g, "").replace(/<|>|%|\\|"|=|'|{|}|!|\?|\(|\)|\*/, "-")
+        change_input.oninput = function(ev) {
+            change_btn.title = change_input.value
+        }
+
+        change_btn.addEventListener('click', function() {
+            let text = change_input.value.replace(/(^\s*)|(\s*$)/g, "").replace(/<|>|%|\\|"|=|'|{|}|!|\?|\(|\)|\*/, "-")
                 //数字待处理
-                let table = $(".select_active").attr('id')
-                let sql = `update ${table} set ${item}='${text}' where ${rootid}='${id}'`
-                ajax("/table?code=update&sentence=" + sql).then((res) => {
-                    if (res.code == 1) {
-                        alert("修改成功!")
-                    } else {
-                        alert("未知错误!")
-                    }
-                })
-
-
-
-
-
-                let newtext = `
+            let table = $(".select_active").attr('id')
+            let sql = `update ${table} set ${item}='${text}' where ${rootid}='${id}'`
+            ajax("/table?code=update&sentence=" + sql).then((res) => {
+                if (res.code == 1) {
+                    alert("修改成功!")
+                } else {
+                    alert("未知错误!")
+                }
+            })
+            let newtext = `
                 <div class="td_container">
                                             <span>${text}</span>
                                             <i class="edit"></i>
                                         </div>`
-                par.children().remove()
-                par.html(newtext)
-            })
+            par.children().remove()
+            par.html(newtext)
+        })
 
-        }
 
     }
 })
 
 
 
-
-
-
-window.onload = function () {
-    getTable("table?code=select&sentence=select * from students")
-    // $(".background").hide()
+//监听窗口大小
+window.onresize = function() {
+        if (document.querySelector(".insert")) {
+            if (innerWidth > 800)
+                $(".insert").attr("colspan", arr.length)
+            if (innerWidth < 800) {
+                if ($(".select_active").html() === "学生")
+                    $(".insert").attr("colspan", 4)
+                if ($(".select_active").html() === "教师")
+                    $(".insert").attr("colspan", 3)
+                if ($(".select_active").html() === "课程")
+                    $(".insert").attr("colspan", 3)
+            }
+        }
+    }
+    //监听hash
+window.onhashchange = function() {
+    if (document.querySelector(".insert")) {
+        if (innerWidth > 800)
+            $(".insert").attr("colspan", arr.length)
+        if (innerWidth < 800) {
+            if ($(".select_active").html() === "学生")
+                $(".insert").attr("colspan", 4)
+            if ($(".select_active").html() === "教师")
+                $(".insert").attr("colspan", 3)
+            if ($(".select_active").html() === "课程")
+                $(".insert").attr("colspan", 3)
+        }
+    }
 }
 
-let id = "cid"
-let name = null;
+window.onload = function() {
+    getTable("")
+}
